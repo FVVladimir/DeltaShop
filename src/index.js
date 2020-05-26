2049695e40639b55652b2db545090871c313f77d
Den <fentau.line@gmail.com>
2020-05-26 22:40:21 +0300

    Create a React app and publish it to GitHub Pages

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Shop :)</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
