da03d7acc791976b2096dbc59a82de750aebc288
Den <fentau.line@gmail.com>
2020-05-27 16:35:14 +0300

    Create next section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-import './index.css';
+import './index.css'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import thunk from 'redux-thunk' // для исинхронности action
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom' // для сайта расположеного на статическом сервере (github pages)
+import { createStore, applyMiddleware } from 'redux'
+import { createBrowserHistory } from 'history'
+import { routerMiddleware } from 'connected-react-router' // СonnectedRouter
+import { composeWithDevTools } from 'redux-devtools-extension' // дэбаг приложения
+
+import createRootReducer from 'reducers'
+import Layout from 'containers/layout'
+
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)] // функции вызываемые между actions & reducers
+const store = createStore( 
+  createRootReducer(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>Shop :)</h1>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <HashRouter history={history}>
+      <Layout />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
-);
+)
+
+// Provider - компонент позволяющий использовать Redux внутри React
+// Layout - компонент в котором находится содержимое приложения
