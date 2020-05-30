4cd9f2e0ad796293888378fbf53be239ef5bfd1a
Vladimir <emirwtf@gmail.com>
2020-05-30 21:01:11 +0300

    Updates

--- precache-manifest.902a28ede59bb29b355af209a568016b.js
+++ precache-manifest.902a28ede59bb29b355af209a568016b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "05f6087a2a8feb6d182e6643a7720088",
+    "revision": "01a3aae303f8fe41b8b7914de8d888e1",
     "url": "/DeltaShop/index.html"
   },
   {
-    "revision": "00ee7e32997b664b163a",
+    "revision": "9747eecb03a0647c5a39",
     "url": "/DeltaShop/static/css/main.5c39fc79.chunk.css"
   },
   {
-    "revision": "54f05cec49fca87b434a",
-    "url": "/DeltaShop/static/js/2.a4ea43d1.chunk.js"
+    "revision": "f38cf4b61dac4d689a9d",
+    "url": "/DeltaShop/static/js/2.4e7dea52.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/DeltaShop/static/js/2.a4ea43d1.chunk.js.LICENSE.txt"
+    "url": "/DeltaShop/static/js/2.4e7dea52.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "00ee7e32997b664b163a",
-    "url": "/DeltaShop/static/js/main.95ce6c41.chunk.js"
+    "revision": "9747eecb03a0647c5a39",
+    "url": "/DeltaShop/static/js/main.07a9ad83.chunk.js"
   },
   {
     "revision": "bd9233e83b39a5b8182c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DeltaShop/precache-manifest.69314fdeaf2f569ab7060844c98beb52.js"
+  "/DeltaShop/precache-manifest.902a28ede59bb29b355af209a568016b.js"
 );
 
 self.addEventListener('message', (event) => {
