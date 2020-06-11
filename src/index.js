5c5fdbce3d94e4cf9db726f1d28f1a811e7f0134
Den <fentau.line@gmail.com>
2020-06-11 13:45:50 +0300

    create next step

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import createRootReducer from 'reducers'
 
 import Grid from '@material-ui/core/Grid'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import Container from '@material-ui/core/Container'
 import { brown } from '@material-ui/core/colors'
 
 const history = createBrowserHistory()
@@ -26,15 +25,13 @@ const store = createStore(
 ReactDOM.render(
   <React.Fragment>
     <CssBaseline />
-    <Container maxWidth='xl' style={{padding: 0, width: '100vw', height: '100vh'}}>
-      <Grid container direction="row" justify="center" alignItems="center" style={{height: '100%', backgroundColor: brown[100]}}>
+    <Grid container direction="row" justify="center" alignItems="center" style={{width: '100%', height: '100vh', backgroundColor: brown[100]}}>
         <Provider store={store}>
           <HashRouter history={history}>
             {routes}
           </HashRouter>
         </Provider>
       </Grid>
-    </Container>
   </React.Fragment>
   ,
   document.getElementById('root')
